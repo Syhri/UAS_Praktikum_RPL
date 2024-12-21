462c013941e7334c82db7992253b55c40303e213
Syhri <fitraabadi12@gmail.com>
2024-12-22 00:58:23 +0800

    add modified

--- obj/Debug/net8.0-windows/Sistem Pemesanan Tiket Kereta.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Sistem Pemesanan Tiket Kereta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistem Pemesanan Tiket Kereta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7033fa52b4aff41160af201abf27fae20a8923f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9770b97c3a39e68ee5a98fdf97e57ae2dd4e805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistem Pemesanan Tiket Kereta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistem Pemesanan Tiket Kereta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
